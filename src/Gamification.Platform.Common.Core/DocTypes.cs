678b6e064bf2c1b25f835135b5eeeeea7bf0880d
Michael Pinkus <mike.pinkus@live.com>
2020-11-10 12:55:32 -0500

    doctypes

---
+++ src/Gamification.Platform.Common.Core/DocTypes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gamification.Platform.Common.Core
+{
+    public enum DocType
+    {
+        Coin = 100,
+        Action = 101,
+        //etc
+    }
+}
