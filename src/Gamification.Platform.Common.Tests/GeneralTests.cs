6bb67b3044dc56242700c72493a5cdc4e0e5f9f9
Michael Pinkus <mike.pinkus@live.com>
2020-11-10 17:44:37 -0500

    Tests

---
+++ src/Gamification.Platform.Common.Tests/GeneralTests.cs
@@ -0,0 +1,171 @@
+﻿using Gamification.Platform.Common.Extensions;
+using Lazlo.Common.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Gamification.Platform.Common.Tests
+{
+    [TestClass]
+    public class GeneralTests
+    {
+        #region Lists
+
+        [TestMethod]
+        public void GoalTriggerExceptionsSucceed()
+        {
+            // Arrange
+            var gTriggers = new GoalTriggers();
+
+            var goalRefId = Guid.NewGuid();
+
+            gTriggers.Add(
+                new GoalTrigger()
+                {
+                    GoalRefId = goalRefId,
+                    Priority = 1,
+                    SimpleName = "Fuu"
+                }
+            );
+
+            // Act 
+
+            // Assert
+
+            // duplicate SimpleName
+            Assert.ThrowsException<ArgumentException>(() =>
+            {
+                gTriggers.Add(
+                    new GoalTrigger()
+                    {
+                        GoalRefId = goalRefId,
+                        Priority = 2,
+                        SimpleName = "Fuu"
+                    }
+                );
+            }
+            );
+
+            // duplicate Priority
+            Assert.ThrowsException<ArgumentException>(() =>
+            {
+                gTriggers.Add(
+                    new GoalTrigger()
+                    {
+                        GoalRefId = goalRefId,
+                        Priority = 1,
+                        SimpleName = "Fuu2"
+                    }
+                );
+            }
+            );
+        }
+
+        [TestMethod]
+        public void TriggerStepsExceptionsSucceed()
+        {
+            // Arrange
+            var entities = new TriggerSteps();
+
+            var goalRefId = Guid.NewGuid();
+
+            entities.Add(
+                new TriggerStep()
+                {
+                    ExecutionOrder = 1,
+                    SimpleName = "Fuu"
+                }
+            );
+
+            // Act 
+
+            // Assert
+
+            // duplicate SimpleName
+            Assert.ThrowsException<ArgumentException>(() =>
+            {
+                entities.Add(
+                new TriggerStep()
+                {
+                    ExecutionOrder = 2,
+                    SimpleName = "Fuu"
+                }
+                );
+            }
+            );
+
+            // duplicate ExecutionOrder
+            Assert.ThrowsException<ArgumentException>(() =>
+            {
+                entities.Add(
+                new TriggerStep()
+                {
+                    ExecutionOrder = 1,
+                    SimpleName = "Fuu2"
+                }
+                );
+            }
+            );
+        }
+
+        #endregion Lists
+
+        #region Tags
+
+        [TestMethod]
+        public void GoalTriggerTagMatchingPlayerTagValidates()
+        {
+            var realmRefId = Guid.NewGuid();
+
+            var player = new Player()
+            {
+                RealmRefId = realmRefId,
+            };
+
+            player.Tags.Add("Fuu");
+
+            var goalTrigger = new GoalTrigger()
+            {
+                GoalRefId = Guid.NewGuid(),
+                SimpleName = "Fuu",
+                Priority = 0,
+                ReleaseOn = DateTimeOffset.UtcNow.AddDays(-365),
+                ExpireOn = DateTimeOffset.MaxValue,
+            };
+
+            goalTrigger.Tags.Add("Fuu");
+
+            Assert.IsTrue(goalTrigger.GoalTriggerTagsAreValid(player));
+        }
+
+        [TestMethod]
+        public void GoalTriggerTagMatchingPlayerTagFails()
+        {
+            var realmRefId = Guid.NewGuid();
+
+            var player = new Player()
+            {
+                RealmRefId = realmRefId,
+            };
+
+            player.Tags.Add("Fuu");
+
+            var goalTrigger = new GoalTrigger()
+            {
+                GoalRefId = Guid.NewGuid(),
+                SimpleName = "Fuu",
+                Priority = 0,
+                ReleaseOn = DateTimeOffset.UtcNow.AddDays(-365),
+                ExpireOn = DateTimeOffset.MaxValue,
+            };
+
+            goalTrigger.Tags.Add("Fuu2");
+
+            Assert.IsFalse(goalTrigger.GoalTriggerTagsAreValid(player));
+        }
+
+        #endregion Tags
+    }
+}
--- src/Gamification.Platform.Common.Tests/GoalTriggerExtensionTests.cs
+++ src/Gamification.Platform.Common.Tests/GoalTriggerExtensionTests.cs
@@ -1,4 +1,11 @@
+using Gamification.Platform.Common.Extensions;
+using Lazlo.Common.Core;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace Gamification.Platform.Common.Tests
 {
@@ -6,8 +13,93 @@ namespace Gamification.Platform.Common.Tests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        [DeploymentItem(@"GeorgiaGeoJsonData.json")]
+        public void SingleActionAwardInsideOfGeoFenceRuleSucceeds()
         {
+            var actionId = Guid.NewGuid().ToString("N");
+
+            var realmRefId = Guid.NewGuid();
+
+            var player = new Player()
+            {
+                RealmRefId = realmRefId,
+            };
+
+            player.Tags.Add("Fuu");
+
+            var actions = new Actions()
+            {
+                new Common.Action()
+                    {
+                        RealmRefId = realmRefId,
+                        ReleasedOn = DateTimeOffset.UtcNow.AddSeconds(-60),
+                        ActionId = actionId,
+                    }
+            };
+
+            var sr = new List<SmartContext<ActionRequest>>()
+            {
+                new SmartContext<ActionRequest>() { Latitude = 33.753746, Longitude = -84.386330, Data = new ActionRequest(){ ActionId = actionId, OccurredOn = DateTimeOffset.UtcNow.AddDays(-10) } },
+                new SmartContext<ActionRequest>() { Latitude = 33.753746, Longitude = -84.386330, Data = new ActionRequest(){ ActionId = actionId, OccurredOn = DateTimeOffset.UtcNow.AddHours(-36) } },
+                new SmartContext<ActionRequest>() { Latitude = 33.753746, Longitude = -84.386330, Data = new ActionRequest(){ ActionId = actionId, OccurredOn = DateTimeOffset.UtcNow.AddHours(-24) } },
+                new SmartContext<ActionRequest>() { Latitude = 33.753746, Longitude = -84.386330, Data = new ActionRequest(){ ActionId = actionId, OccurredOn = DateTimeOffset.UtcNow.AddHours(-8) } },
+                new SmartContext<ActionRequest>() { Latitude = 33.753746, Longitude = -84.386330, Data = new ActionRequest(){ ActionId = actionId, OccurredOn = DateTimeOffset.UtcNow.AddMinutes(-1) } }
+            };
+
+            NetTopologySuite.Geometries.MultiPolygon insideOf = null;
+
+            var jsonSerializer = NetTopologySuite.IO.GeoJsonSerializer.Create();
+
+            using (StreamReader file = File.OpenText(@"GeorgiaGeoJsonData.json"))
+            using (JsonTextReader reader = new JsonTextReader(file))
+            {
+                insideOf = jsonSerializer.Deserialize<NetTopologySuite.Geometries.MultiPolygon>(reader);
+            }
+
+            var coins = new Coins();
+            coins.LoadTestData(realmRefId);
+
+            var awards = new Awards();
+            awards.LoadTestData(coins);
+
+            var goal = new Goal();
+            goal.LoadTestData(realmRefId, awards);
+
+            var goalTriggers = new GoalTriggers();
+            goalTriggers.LoadTestData(realmRefId, goal, actions, insideOf, null, null);
+
+            //TODO move to stand alone test
+            //var json = JsonConvert.SerializeObject(goalTriggers);
+
+            //var t2 = JsonConvert.DeserializeObject<GoalTriggers>(json, new NetTopologySuite.IO.Converters.GeometryConverter());
+
+            foreach (var goalTrigger in goalTriggers)
+            {
+                var events = new PlayerActionEvents();
+                events.AddRange(
+                    sr.Select(e =>
+                        new PlayerActionEvent()
+                        {
+                            OccurredAt = new NetTopologySuite.Geometries.Point(e.Longitude, e.Latitude)
+                        }.Map(e.Data, actions)
+                    )
+                );
+
+                var qualifyingActionRequests = goalTrigger.ValidateSteps(events);
+
+                Assert.IsTrue(qualifyingActionRequests.Count > 0);
+            }
+
+
+            // the action is "read an ad"
+            // step 1 read an ad on tuesday
+            // step 2 read an ad on thursday
+
+
+
+            // convert requests to Events
+
+
         }
     }
 }
