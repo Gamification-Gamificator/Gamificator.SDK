5e8528f5006608f2e3b761fe64a0a6c277699312
Michael Pinkus <mike.pinkus@live.com>
2020-11-11 17:18:17 -0500

    promotions and ActionOccurrenceRules

---
+++ src/Gamification.Platform.Common.Core/Enums/Enums.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gamification.Platform.Common.Core.Enums
+{
+    public enum OperationRuleType
+    {
+        None = 0,
+        And = 1,
+        Or = 2,
+    }
+
+    public enum TenseRuleType
+    {
+        None = 0,
+        Did = 1,
+        DidNot = 2,
+    }
+
+    public enum CompareRuleType
+    {
+        None = 0,
+        Greater = 1,
+        GreaterOrEqual = 2,
+        Less = 3,
+        LessOrEqual = 4,
+    }
+}
--- src/Gamification.Platform.Common.Core/TriggerStep.cs
+++ src/Gamification.Platform.Common.Core/TriggerStep.cs
@@ -5,7 +5,7 @@ using System;
 using System.Collections.Generic;
 
 namespace Gamification.Platform.Common.Core
-{    
+{
     public class TriggerStepCore
     {
         /// <summary>
@@ -20,32 +20,5 @@ namespace Gamification.Platform.Common.Core
         [JsonProperty(PropertyName = "nameTranslations")]
         public List<StringTranslation> NameTranslations { get; set; } = new List<StringTranslation>();
 
-        /// <summary>
-        /// Step requires Actions to have occurred inside these Polygons
-        /// </summary>
-        [JsonProperty(PropertyName = "insideOf")]
-        /// <summary>
-        [JsonConverter(typeof(NetTopologySuiteGeometryConverter))]
-        public MultiPolygon InsideOf { get; set; }
-
-        /// <summary>
-        /// Step requires Actions to have occurred outside these Polygons
-        /// </summary>
-        [JsonProperty(PropertyName = "outsideOf")]
-        [JsonConverter(typeof(NetTopologySuiteGeometryConverter))]
-        public MultiPolygon OutsideOf { get; set; }
-
-        /// <summary>
-        /// True: this TriggerStep calculates since the last ActionCheckpoint
-        /// False: this TriggerStep calculates All Actions
-        /// </summary>
-        [JsonProperty(PropertyName = "areActionsCheckpointed")]
-        public bool AreActionsCheckpointed { get; set; }
-
-        /// <summary>
-        /// You must perform one or more [count] Actions to achieve this TriggerStep
-        /// </summary>
-        [JsonProperty(PropertyName = "actions")]
-        public Dictionary<Guid, int> Actions { get; set; } = new Dictionary<Guid, int>();
     }
 }
