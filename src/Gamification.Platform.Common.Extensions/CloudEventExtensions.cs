fb714ad3e9f2b265027674f60f433f13e82a057c
Michael Pinkus <mike.pinkus@live.com>
2020-11-13 14:48:52 -0500

    refactor

--- src/Gamification.Platform.Common.Extensions/CloudEventExtensions.cs
+++ src/Gamification.Platform.Common.Extensions/CloudEventExtensions.cs
@@ -11,12 +11,11 @@ namespace Gamification.Platform.Common.Extensions
 {
     public static class CloudEventExtensions
     {
-        public static CloudEvent ToCloudEvent<T>(this SmartRequest<T> request, string type, string subject, Uri uri)
+        public static CloudEvent ToCloudEvent<T>(this SmartRequest<T> request, string subject, Uri uri)
         {
             return new CloudEvent(request.Data.GetType().ToString(), uri)
             {
                 SpecVersion = CloudEventsSpecVersion.V1_0,
-                Type = type,
                 Subject = subject,
                 Time = DateTime.UtcNow,
                 Id = Guid.NewGuid().ToString(),
@@ -25,12 +24,11 @@ namespace Gamification.Platform.Common.Extensions
             };
         }
 
-        public static CloudEvent ToCloudEvent<T>(this SmartContext<T> ctx, string type, string subject, Uri uri)
+        public static CloudEvent ToCloudEvent<T>(this SmartContext<T> ctx, string subject, Uri uri)
         {
             return new CloudEvent(ctx.Data.GetType().ToString(), uri)
             {
                 SpecVersion = CloudEventsSpecVersion.V1_0,
-                Type = type,
                 Subject = subject,
                 Time = DateTime.UtcNow,
                 Id = Guid.NewGuid().ToString(),
