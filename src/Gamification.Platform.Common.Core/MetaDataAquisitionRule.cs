3da1b206ec202d30d6eabcbf8c2369d6ceb6eacc
Michael Pinkus <mike.pinkus@live.com>
2020-11-14 15:18:09 -0500

    MetaDataAquisition scaffolding

---
+++ src/Gamification.Platform.Common.Core/MetaDataAquisitionRule.cs
@@ -0,0 +1,49 @@
+﻿using Gamification.Platform.Common.Core;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace Gamification.Platform.Common.Core
+{
+    // this allows meta data to be aquired outside the scope of an App
+    // so a teacher could cause meta data to be aquired and Gamifyit...
+
+    public class MetaDataAquisitionRuleCore
+    {
+        /// <summary>
+        /// Name, Birth Date, etc
+        /// </summary>
+        [JsonProperty(PropertyName = "simpleName")]
+        public string SimpleName { get; set; }
+
+        /// <summary>
+        /// Étoile d’or ! OR Gold Stern!
+        /// </summary>
+        [JsonProperty(PropertyName = "nameTranslations")]
+        public List<StringTranslation> NameTranslations { get; set; } = new List<StringTranslation>();
+
+        /// <summary>
+        /// Étoile d’or ! OR Gold Stern!
+        /// </summary>
+        [JsonProperty(PropertyName = "descriptionTranslations")]
+        public List<StringTranslation> DescriptionTranslations { get; set; } = new List<StringTranslation>();
+
+        /// <summary>
+        /// Trophy image or video etc
+        /// </summary>
+        [JsonProperty(PropertyName = "mediaTranslations")]
+        public List<MediaTranslation> MediaTranslations { get; set; } = new List<MediaTranslation>();
+
+        [JsonProperty(PropertyName = "releaseOn")]
+        public DateTimeOffset ReleaseOn { get; set; } = DateTimeOffset.MinValue;
+
+        [JsonProperty(PropertyName = "releasedOn")]
+        public DateTimeOffset? ReleasedOn { get; set; }
+
+        [JsonProperty(PropertyName = "expireOn")]
+        public DateTimeOffset ExpireOn { get; set; } = DateTimeOffset.MaxValue;
+
+        [JsonProperty(PropertyName = "expiredOn")]
+        public DateTimeOffset? ExpiredOn { get; set; }
+    }
+}
