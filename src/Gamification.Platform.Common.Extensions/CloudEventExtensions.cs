4a629051ad5c7d2a518df78e69a2ed43f0a2f481
Michael Pinkus <mike.pinkus@live.com>
2020-11-18 10:23:38 -0500

    cloudevents promoted

--- src/Gamification.Platform.Common.Extensions/CloudEventExtensions.cs
+++ src/Gamification.Platform.Common.Extensions/CloudEventExtensions.cs
@@ -1,38 +1,37 @@
-﻿using CloudNative.CloudEvents;
-using Lazlo.Common;
-using Lazlo.Common.Requests;
-using Newtonsoft.Json;
-using System;
-using System.Net.Mime;
+﻿//using Lazlo.Common;
+//using Lazlo.Common.Requests;
+//using Newtonsoft.Json;
+//using System;
+//using System.Net.Mime;
 
-namespace Gamification.Platform.Common.Extensions
-{
-    public static class CloudEventExtensions
-    {
-        public static CloudEvent ToCloudEvent<T>(this SmartRequest<T> request, string subject, Uri uri)
-        {
-            return new CloudEvent(request.Data.GetType().ToString(), uri)
-            {
-                SpecVersion = CloudEventsSpecVersion.V1_0,
-                Subject = subject,
-                Time = DateTime.UtcNow,
-                Id = Guid.NewGuid().ToString(),
-                DataContentType = new ContentType("application/json"),
-                Data = JsonConvert.SerializeObject(request)
-            };
-        }
+//namespace Gamification.Platform.Common.Extensions
+//{
+//    public static class CloudEventExtensions
+//    {
+//        public static CloudEvent ToCloudEvent<T>(this SmartRequest<T> request, string subject, Uri uri)
+//        {
+//            return new CloudEvent(request.Data.GetType().ToString(), uri)
+//            {
+//                SpecVersion = CloudEventsSpecVersion.V1_0,
+//                Subject = subject,
+//                Time = DateTime.UtcNow,
+//                Id = Guid.NewGuid().ToString(),
+//                DataContentType = new ContentType("application/json"),
+//                Data = JsonConvert.SerializeObject(request)
+//            };
+//        }
 
-        public static CloudEvent ToCloudEvent<T>(this SmartContext<T> ctx, string subject, Uri uri)
-        {
-            return new CloudEvent(ctx.Data.GetType().ToString(), uri)
-            {
-                SpecVersion = CloudEventsSpecVersion.V1_0,
-                Subject = subject,
-                Time = DateTime.UtcNow,
-                Id = Guid.NewGuid().ToString(),
-                DataContentType = new ContentType("application/json"),
-                Data = JsonConvert.SerializeObject(ctx)
-            };
-        }
-    }
-}
+//        public static CloudEvent ToCloudEvent<T>(this SmartContext<T> ctx, string subject, Uri uri)
+//        {
+//            return new CloudEvent(ctx.Data.GetType().ToString(), uri)
+//            {
+//                SpecVersion = CloudEventsSpecVersion.V1_0,
+//                Subject = subject,
+//                Time = DateTime.UtcNow,
+//                Id = Guid.NewGuid().ToString(),
+//                DataContentType = new ContentType("application/json"),
+//                Data = JsonConvert.SerializeObject(ctx)
+//            };
+//        }
+//    }
+//}
