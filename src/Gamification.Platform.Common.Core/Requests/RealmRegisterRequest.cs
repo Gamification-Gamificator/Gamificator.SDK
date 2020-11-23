2fdf5c01f65c2a0403626eb3aaf1cd0385bfd705
Michael Pinkus <mike.pinkus@live.com>
2020-11-23 15:42:45 -0500

    request

---
+++ src/Gamification.Platform.Common.Core/Requests/RealmRegisterRequest.cs
@@ -0,0 +1,12 @@
+﻿using Gamification.Platform.Common.Core;
+using Lazlo.Common.Requests;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gamification.Platform.Common.Requests
+{
+    public sealed class RealmRegisterRequest : RealmCore
+    {
+    }
+}
