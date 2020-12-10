57114cae87fd08adb7468bca464fb7c0866f014d
Mike Pinkus <mike@lazlo.io>
2020-12-10 15:52:24 -0500

    promotions

---
+++ src/Gamification.Platform.Common.Core/ActionEventBase.cs
@@ -0,0 +1,25 @@
+﻿using Gamification.Platform.Common.Core;
+using NetTopologySuite.Geometries;
+using Newtonsoft.Json;
+using System;
+
+namespace Gamification.Platform.Common.Core
+{
+    /// <summary>
+    /// A true Base, Core is an abstraction for .Common vs .Display
+    /// </summary>
+    public class ActionEventCore : OccurrenceBase
+    {
+        [JsonRequired]
+        [JsonProperty(PropertyName = "actionRefId")]
+        public Guid ActionRefId { get; set; }
+
+        /// <summary>
+        /// string descibing Hierarchical relationships
+        /// Example: 2020:Fall:12
+        /// Example: Year:Season:Game#
+        /// </summary>
+        [JsonProperty(PropertyName = "sessionHierarchy")]
+        public string SessionHierarchy { get; set; } = "default";
+    }
+}
---
+++ src/Gamification.Platform.Common.Core/CoinMultiplierRule.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace Gamification.Platform.Common.Core
+{
+    public class CoinMultiplierRuleCore
+    {
+        [JsonProperty(PropertyName = "multiplier")]
+        public decimal Multiplier { get; set; }
+    }
+}
