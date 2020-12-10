7606abec45e8a9e891b968037aea1813440b14d2
Mike Pinkus <mike@lazlo.io>
2020-12-10 13:20:23 -0500

    promotion

---
+++ src/Gamification.Platform.Common.Core/OccurrenceBase.cs
@@ -0,0 +1,22 @@
+﻿using Gamification.Platform.Common.Core;
+using NetTopologySuite.Geometries;
+using Newtonsoft.Json;
+using System;
+
+namespace Gamification.Platform.Common.Core
+{
+    public abstract class OccurrenceBase
+    {
+        /// <summary>
+        /// An xRequest may occur out of sync with the x
+        /// This is WHEN the x Occurred NOT when it was sent
+        /// </summary>
+        [JsonRequired]
+        [JsonProperty(PropertyName = "occurredOn")]
+        public DateTimeOffset OccurredOn { get; set; }
+
+        [JsonProperty(PropertyName = "occurredAt")]
+        [JsonConverter(typeof(NetTopologySuiteGeometryPointConverter))]
+        public Point OccurredAt { get; set; }
+    }
+}
