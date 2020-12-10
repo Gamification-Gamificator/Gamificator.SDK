949283728e2a99248ab470330a5619cd6c838083
Mike Pinkus <mike@lazlo.io>
2020-12-10 13:26:51 -0500

    goalevent

---
+++ src/Gamification.Platform.Common.Display/GoalEvent.cs
@@ -0,0 +1,34 @@
+﻿using Gamification.Platform.Common.Core;
+using Newtonsoft.Json;
+using System;
+
+namespace Gamification.Platform.Common.Display
+{
+    /// <summary>
+    /// The persisted Goal transaction
+    /// </summary>
+    public class GoalEvent : OccurrenceBase
+    {
+        [JsonRequired]
+        [JsonProperty(PropertyName = "playerRefId")]
+        public Guid PlayerRefId { get; set; }
+
+        /// <summary>
+        /// All root entities require RealmRefId for multi-tenancy
+        /// </summary>
+        [JsonRequired]
+        [JsonProperty(PropertyName = "realmRefId")]
+        public Guid RealmRefId { get; set; }
+
+        [JsonRequired]
+        [JsonProperty(PropertyName = "goalRefId")]
+        public Guid GoalRefId { get; set; }
+
+        /// <summary>
+        /// A Goal is accomplished
+        /// </summary>
+        [JsonRequired]
+        [JsonProperty(PropertyName = "accomplishedOn")]
+        public DateTimeOffset AccomplishedOn { get; set; }
+    }
+}
