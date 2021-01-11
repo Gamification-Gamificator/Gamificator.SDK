00d6c04c150d1b39ac5a7113ea6da441cb58dbcc
Vijay Narayan <vijay.narayan@lazlo.io>
2021-01-11 13:49:18 -0500

    1. Added PlayerStatisticsResponse 2. Added Gamification.Platform.Common.Display nuget reference to Gamification.Platform.Common.csproj

---
+++ src/Gamification.Platform.Common/Responses/Responses.cs
@@ -0,0 +1,12 @@
+﻿using Gamification.Platform.Common.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Gamification.Platform.Common.Display;
+
+namespace Gamification.Platform.Common.Responses
+{
+    public sealed class PlayerStatisticsResponse : PlayerAwardDisplay
+    {
+    }
+}
