66e494dce7b4ad84212b8e8ab22adb1ea9ec0210
Vijay Narayan <vijay.narayan@lazlo.io>
2021-02-10 14:13:40 -0500

    Added PushNotificationMetadata

---
+++ src/Gamification.Platform.Common/PushNotificationMetadata.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Gamification.Platform.Common
+{
+    public class PushNotificationMetadata
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "eventTime")]
+        public DateTimeOffset EventTime { get; set; }
+
+        [JsonProperty(PropertyName = "title")]
+        public string Title { get; set; }
+
+        [JsonProperty(PropertyName = "message")]
+        public string Message { get; set; }
+
+        [JsonProperty(PropertyName = "url")]
+        public string Url { get; set; }
+
+        [JsonProperty(PropertyName = "playerRefId")]
+        public Guid PlayerRefId { get; set; }
+
+        [JsonProperty(PropertyName = "expireOn")]
+        public DateTimeOffset ExpireOn { get; set; }
+    }
+}
