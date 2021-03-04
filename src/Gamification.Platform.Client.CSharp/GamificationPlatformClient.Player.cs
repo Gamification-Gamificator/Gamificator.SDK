818f342cec7fd240ee63ce2aff4707ee4de85f35
Vijay Narayan <vijay.narayan@lazlo.io>
2021-03-04 17:52:30 -0500

    Updated player profile APIs

---
+++ src/Gamification.Platform.Client.CSharp/GamificationPlatformClient.Player.cs
@@ -0,0 +1,125 @@
+﻿using Gamification.Platform.Common;
+using Lazlo.Common.Responses;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Gamification.Platform.SDK.CSharp
+{
+    public partial class GamificationPlatformClient
+    {
+        public async Task<PlayerProfile> RetrievePlayerProfileByIdAsync(Guid correlationRefId, Guid playerRefId, CancellationToken cancellationToken = default)
+        {
+            HttpResponseMessage httpResponse = await SendAsJsonAsync(
+                            method: HttpMethod.Get,
+                            pathAndQuery: $"api/v1/player/{playerRefId}",
+                            correlationRefId: correlationRefId,
+                            request: null,
+                            requestHeaders: null,
+                            cancellationToken).ConfigureAwait(false);
+
+            string responseJson = await httpResponse.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+            var response = JsonConvert.DeserializeObject<SmartResponse<PlayerProfile>>(responseJson);
+
+            if (httpResponse.IsSuccessStatusCode)
+            {
+                return response.Data;
+            }
+
+            throw new Exception($"Get Player Profile by Id failed. {response.Error.Message}");
+        }
+
+        public async Task<PlayerProfile> RetrievePlayerProfileByEmailAsync(Guid correlationRefId, string email, CancellationToken cancellationToken = default)
+        {
+            HttpResponseMessage httpResponse = await SendAsJsonAsync(
+                            method: HttpMethod.Get,
+                            pathAndQuery: $"api/v1/player/email/{email}",
+                            correlationRefId: correlationRefId,
+                            request: null,
+                            requestHeaders: null,
+                            cancellationToken).ConfigureAwait(false);
+
+            string responseJson = await httpResponse.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+            var response = JsonConvert.DeserializeObject<SmartResponse<PlayerProfile>>(responseJson);
+
+            if (httpResponse.IsSuccessStatusCode)
+            {
+                return response.Data;
+            }
+
+            throw new Exception($"Get Player Profile by email failed. {response.Error.Message}");
+        }
+
+        public async Task<PlayerProfile> CreatePlayerProfileAsync(Guid correlationRefId, PlayerProfile playerProfile, CancellationToken cancellationToken = default)
+        {
+            HttpResponseMessage httpResponse = await SendAsJsonAsync(
+                            method: HttpMethod.Post,
+                            pathAndQuery: $"api/v1/player",
+                            correlationRefId: correlationRefId,
+                            request: playerProfile,
+                            requestHeaders: null,
+                            cancellationToken).ConfigureAwait(false);
+
+            string responseJson = await httpResponse.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+            var response = JsonConvert.DeserializeObject<SmartResponse<PlayerProfile>>(responseJson);
+
+            if (httpResponse.IsSuccessStatusCode)
+            {
+                return response.Data;
+            }
+
+            throw new Exception($"Create Player Profile failed. {response.Error.Message}");
+        }
+
+        public async Task UpdatePlayerProfileAsync(Guid correlationRefId, PlayerProfile contact, CancellationToken cancellationToken = default)
+        {
+            HttpResponseMessage httpResponse = await SendAsJsonAsync(
+                            method: HttpMethod.Put,
+                            pathAndQuery: $"api/v1/player",
+                            correlationRefId: correlationRefId,
+                            request: contact,
+                            requestHeaders: null,
+                            cancellationToken).ConfigureAwait(false);
+
+            string responseJson = await httpResponse.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+            var response = JsonConvert.DeserializeObject<SmartResponse<string>>(responseJson);
+
+            if (httpResponse.IsSuccessStatusCode)
+            {
+                return;
+            }
+
+            throw new Exception($"Update Player failed. {response.Error.Message}");
+        }
+
+        public async Task DeletePlayerAsync(Guid correlationRefId, Guid playerRefId, CancellationToken cancellationToken = default)
+        {
+            HttpResponseMessage httpResponse = await SendAsJsonAsync(
+                            method: HttpMethod.Delete,
+                            pathAndQuery: $"api/v1/player/{playerRefId}",
+                            correlationRefId: correlationRefId,
+                            request: null,
+                            requestHeaders: null,
+                            cancellationToken).ConfigureAwait(false);
+
+            string responseJson = await httpResponse.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+            var response = JsonConvert.DeserializeObject<SmartResponse<string>>(responseJson);
+
+            if (httpResponse.IsSuccessStatusCode)
+            {
+                return;
+            }
+
+            throw new Exception($"Delete Player failed. {response.Error.Message}");
+        }
+    }
+}
