a8f70b5cde984832f25ecf98c978b4dc6777bb97
Michael Pinkus <mike.pinkus@live.com>
2021-03-28 15:05:06 -0400

    PlayerSummaryDisplay

---
+++ src/Gamification.SDK.Display/CoinBalanceDisplay.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace Gamification.SDK.Display
+{
+    public class CoinBalanceDisplay
+    {
+        [JsonProperty(PropertyName = "coin")]
+        public CoinDisplay Coin { get; set; }
+
+        [JsonProperty(PropertyName = "balance")]
+        public decimal Balance { get; set; }
+
+        [JsonProperty(PropertyName = "asOf")]
+        public DateTimeOffset AsOf { get; set; } = DateTimeOffset.UtcNow;
+    }
+
+    public class CoinBalancesDisplay : List<CoinBalanceDisplay>
+    {
+
+    }
+}
---
+++ src/Gamification.SDK.Display/PlayerSummaryDisplay.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gamification.SDK.Display
+{
+    public class PlayerSummaryDisplay
+    {
+        [JsonProperty(PropertyName = "id")]
+        public Guid EntityRefId { get; set; }
+
+        [JsonProperty(PropertyName = "goals")]
+        public GoalDisplays Goals { get; set; }
+
+        [JsonProperty(PropertyName = "achievements")]
+        public AchievementDisplays Achievements { get; set; }
+
+        [JsonProperty(PropertyName = "coinBalances")]
+        public CoinBalancesDisplay CoinBalances { get; set; }
+    }
+}
