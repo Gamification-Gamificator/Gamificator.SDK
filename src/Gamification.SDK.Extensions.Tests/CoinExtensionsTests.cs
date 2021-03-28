0705cb364aa006305a4d52b8b5a82388708b9f9e
Michael Pinkus <mike.pinkus@live.com>
2021-03-28 15:33:47 -0400

    mock and tests

---
+++ src/Gamification.SDK.Extensions.Tests/CoinExtensionsTests.cs
@@ -0,0 +1,24 @@
+﻿using Gamification.SDK.Display;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gamification.SDK.Mock.Data
+{
+    [TestClass]
+    public class CoinExtensions
+    {
+        [TestMethod]
+        public void TestCoinBalanceMocking()
+        {
+            var uri = new Uri("https://rprcorporate.blob.core.windows.net/media/sampletrophy.png?sp=rl&st=2021-03-28T19:06:34Z&se=2025-03-29T19:06:00Z&sv=2020-02-10&sr=b&sig=M3oKCysFeKGWw7jDnn1kCFNTFHdqxoOtKVzVFIYXXZs%3D");
+
+            var cbd = new CoinBalanceDisplay().ToMock(uri);
+            Assert.IsTrue(cbd.Balance > 0.0M);
+
+            var a1 = new CoinDisplay().ToMock(uri);
+            Assert.IsTrue(!string.IsNullOrEmpty(a1.SimpleName));
+        }
+    }
+}
---
+++ src/Gamification.SDK.Mock.Data/CoinExtensions.cs
@@ -0,0 +1,73 @@
+﻿using Gamification.SDK.Display;
+using NLipsum.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ThreeTwoSix.Common;
+using ThreeTwoSix.Core;
+using ThreeTwoSix.Extensions.Mock.Data;
+using ThreeTwoSix.SDK.Extensions;
+
+namespace Gamification.SDK.Mock.Data
+{
+    public static class CoinBalanceExtensions
+    {
+        public static IEnumerable<CoinBalanceDisplay> ToMock(this CoinBalancesDisplay coinBalanceDisplay, Uri uri, int count = 1)
+        {
+            var rnd = new Random(Guid.NewGuid().GetHashCode());
+            string rawText = Lipsums.LoremIpsum;
+            LipsumGenerator lipsum = new LipsumGenerator(rawText, false);
+            var cCount = rnd.Next(6);
+
+            for (int i = 0; i < count; i++)
+            {
+                var last = $"{lipsum.GenerateWords(1)[0]}";
+                var first = $"{lipsum.GenerateWords(1)[0]}";
+                var profile = $"{first}.{last}.{Guid.NewGuid().ToString().Substring(4)}".ToLower();
+
+                coinBalanceDisplay.Add(
+                    new CoinBalanceDisplay().ToMock(uri)
+                    );
+            }
+
+            return coinBalanceDisplay;
+        }
+
+        public static CoinBalanceDisplay ToMock(this CoinBalanceDisplay cb, Uri uri)
+        {
+            var rnd = new Random(Guid.NewGuid().GetHashCode());
+            string rawText = Lipsums.LoremIpsum;
+            LipsumGenerator lipsum = new LipsumGenerator(rawText, false);
+
+            var last = $"{lipsum.GenerateWords(1)[0]}";
+            var first = $"{lipsum.GenerateWords(1)[0]}";
+            var profile = $"{first}.{last}.{Guid.NewGuid().ToString("N").Substring(4)}".ToLower();
+
+            return new CoinBalanceDisplay()
+            {
+                Balance = rnd.Next(1, 100),
+                AsOf = DateTimeOffset.UtcNow,
+                Coin = new CoinDisplay().ToMock(uri)
+            };
+        }
+
+        public static CoinDisplay ToMock(this CoinDisplay cb, Uri uri)
+        {
+            var rnd = new Random(Guid.NewGuid().GetHashCode());
+            string rawText = Lipsums.LoremIpsum;
+            LipsumGenerator lipsum = new LipsumGenerator(rawText, false);
+
+            var last = $"{lipsum.GenerateWords(1)[0]}";
+            var first = $"{lipsum.GenerateWords(1)[0]}";
+            var profile = $"{first}.{last}.{Guid.NewGuid().ToString("N").Substring(4)}".ToLower();
+
+            return new CoinDisplay()
+            {
+                EntityRefId = Guid.NewGuid(),
+                SimpleName = $"{lipsum.GenerateWords(1)[0]}",
+                NameTranslations = new StringTranslationsCore().Map(new StringTranslations().ToMock()),
+                MediaTranslations = new MediaTranslationsCore().Map(new MediaTranslations().ToMock(uri))
+            };
+        }
+    }
+}
