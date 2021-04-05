f67d1a28179056e44ccdc34b4c2fda168d322115
Vijay Narayan <vijay.narayan@lazlo.io>
2021-04-05 10:35:52 -0700

    Added Mocks for PlayerAward(s)

---
+++ src/Gamification.SDK.Extensions.Tests/PlayerAwardExtensionsTests.cs
@@ -0,0 +1,31 @@
+﻿using Gamification.SDK.Display;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gamification.SDK.Mock.Data
+{
+    [TestClass]
+    public class PlayerAwardExtensionsTests
+    {
+        [TestMethod]
+        public void TestPlayerAwardMocking()
+        {
+            var uri = new Uri("https://rprcorporate.blob.core.windows.net/media/sampletrophy.png?sp=rl&st=2021-03-28T19:06:34Z&se=2025-03-29T19:06:00Z&sv=2020-02-10&sr=b&sig=M3oKCysFeKGWw7jDnn1kCFNTFHdqxoOtKVzVFIYXXZs%3D");
+
+            var pad = new PlayerAwardDisplay().ToMock(uri);
+            Assert.IsTrue(!string.IsNullOrEmpty(pad.Description));
+            Assert.IsTrue(pad.Value == 400);
+        }
+
+        [TestMethod]
+        public void TestPlayerAwardsMocking()
+        {
+            var uri = new Uri("https://rprcorporate.blob.core.windows.net/media/sampletrophy.png?sp=rl&st=2021-03-28T19:06:34Z&se=2025-03-29T19:06:00Z&sv=2020-02-10&sr=b&sig=M3oKCysFeKGWw7jDnn1kCFNTFHdqxoOtKVzVFIYXXZs%3D");
+
+            var pad = new PlayerAwardDisplays().ToMock(uri, 2);
+            Assert.IsTrue(pad.Count == 2);
+        }
+    }
+}
---
+++ src/Gamification.SDK.Mock.Data/PlayerAwardExtensions.cs
@@ -0,0 +1,58 @@
+﻿using Gamification.SDK.Display;
+using NLipsum.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ThreeTwoSix.Common;
+using ThreeTwoSix.Core;
+using ThreeTwoSix.Extensions.Mock.Data;
+using ThreeTwoSix.SDK.Extensions;
+
+namespace Gamification.SDK.Mock.Data
+{
+    public static class PlayerAwardExtensions
+    {
+        public static PlayerAwardDisplay ToMock(this PlayerAwardDisplay pad, Uri uri)
+        {
+            string rawText = Lipsums.LoremIpsum;
+            LipsumGenerator lipsum = new LipsumGenerator(rawText, false);
+
+            return new PlayerAwardDisplay()
+            {
+                Coin = new CoinDisplay().ToMock(uri),
+                TriggeringGoal = new GoalDisplay().ToMock(uri),
+                TriggeringAchievement = new AchievementDisplay().ToMock(uri),
+                TriggeringAction = new ActionDisplay().ToMock(uri),
+                Description = $"{lipsum.GenerateWords(1)[0]}",
+                Value = 400
+            };
+        }
+
+        public static PlayerAwardDisplays ToMock(this PlayerAwardDisplays pads, Uri uri, int count = 1)
+        {
+            var rnd = new Random(Guid.NewGuid().GetHashCode());
+            string rawText = Lipsums.LoremIpsum;
+            LipsumGenerator lipsum = new LipsumGenerator(rawText, false);
+
+            for (int i = 0; i < count; i++)
+            {
+                var last = $"{lipsum.GenerateWords(1)[0]}";
+                var first = $"{lipsum.GenerateWords(1)[0]}";
+                var profile = $"{first}.{last}.{Guid.NewGuid().ToString().Substring(4)}".ToLower();
+
+                pads.Add(
+                    new PlayerAwardDisplay()
+                    {
+                        Coin = new CoinDisplay().ToMock(uri),
+                        TriggeringGoal = new GoalDisplay().ToMock(uri),
+                        TriggeringAchievement = new AchievementDisplay().ToMock(uri),
+                        TriggeringAction = new ActionDisplay().ToMock(uri),
+                        Description = $"{lipsum.GenerateWords(1)[0]}",
+                        Value = 400
+                    });
+            }
+
+            return pads;
+        }
+    }
+}
